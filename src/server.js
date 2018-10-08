58ee8201efb71e70b59b6efe94ad1ea746f96d23
Alexandre973 <alexandre.robert57@gmail.com>
2018-10-08 17:39:50 +0200

    Add page

--- src/server.js
+++ src/server.js
@@ -5,10 +5,14 @@ let app = express()
 app.set('view engine', 'twig');
 app.set("views", "./src/views");
 
-app.use('/assets',express.static('public'))
+app.use('/assets', express.static('public'))
 
 app.get('/', (request, response) => {
-    response.render('index', {test: 'Salut'})
+    response.render('index', { test: 'Salut' })
+})
+
+app.get('/cluedo', (request, response) => {
+    response.render('cluedo')
 })
 
 app.listen(8080)
