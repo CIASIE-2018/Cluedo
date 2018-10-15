e98decd4fffe8a1dfbac46488b41daab562978ea
Richard Lemmer <fullpower57940@gmail.com>
2018-10-15 17:56:15 +0200

    modif

--- src/controller/GridController.js
+++ src/controller/GridController.js
@@ -6,19 +6,19 @@ class GridController {
             let x = 0;
             let y = 0;
             this.grid= new Array(25);
-            grid[y]= new Array(25)
+            this.grid[y]= new Array(25)
             while(i<contents.length){
                 if(contents[i]=='\n'){
                     y++;
-                    grid[y]= new Array(x)
+                    this.grid[y]= new Array(x)
                     x=0;
                 }else{
-                    grid[y][x] = contents[i];
+                    this.grid[y][x] = contents[i];
                     x++;
                 }
                 i++;
             }
-            grid.forEach(element => {
+            this.grid.forEach(element => {
                 console.log(element.toString())
             });
     }
