0db3971c8ab58b14d5bae9b21bea1e8d6cb0e39f
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2018-10-17 21:19:45 +0200

    Modification du module de session

--- src/model/PlayerModel.js
+++ src/model/PlayerModel.js
@@ -16,3 +16,5 @@ class Player {
     this.name = name;
   }
 }
+
+module.exports = Player;
--- src/service/session.js
+++ src/service/session.js
@@ -1,11 +1,12 @@
-const Player = require("../model/PlayerModel");
-const uuidv4 = require("uuid/v4");
-
 var session = (req, res, next) => {
+  const Player = require("../model/PlayerModel");
+  const uuidv4 = require("uuid/v4");
+
   // si le navigateur n'a pas de session, on lui crée une session contenant le player
   if (req.session.player === undefined) {
     let player = new Player(uuidv4(), null);
     req.session.player = player;
+    console.log("New player : " + player.getUid());
   }
   next();
 };
