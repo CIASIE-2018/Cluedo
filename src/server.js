ddb21f356b870f473b6deb9bbf83a2aa2c06ecd5
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2018-10-17 19:02:55 +0200

    Utilisation du fichier de configuration dans le server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,24 @@
-let Cluedo = require('./controller/CluedoController');
-let Grid = require('./controller/GridController');
-let express = require('express')
+let Cluedo = require("./controller/CluedoController");
+let Grid = require("./controller/GridController");
+let express = require("express");
 
-let app = express()
+const config = require("./config.json");
 
-app.set('view engine', 'twig');
+let app = express();
+
+app.set("view engine", "twig");
 app.set("views", "./src/views");
 
-app.use('/assets', express.static('public'))
+app.use(config.ressources.staticFilesRootPath, express.static("public"));
 
-app.get('/', (request, response) => {
-    response.render('index')
-})
+app.get("/", (request, response) => {
+  response.render("index");
+});
 
-app.get('/cluedo', (request, response) => {
-    var grid = new Grid().grid;
-    Cluedo.start();
-    response.render('cluedo', { grid });
-})
+app.get("/cluedo", (request, response) => {
+  var grid = new Grid().grid;
+  Cluedo.start();
+  response.render("cluedo", { grid });
+});
 
-
-app.listen(8080)
+app.listen(config.app.port);
