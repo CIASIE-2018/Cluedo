236dcb4b044d9b997691e8766e34d75482c1d1b5
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2018-10-17 20:51:58 +0200

    création du model Player

--- src/model/PlayerModel.js
+++ src/model/PlayerModel.js
@@ -1,1 +1,18 @@
-//stand by
+class Player {
+  constructor(uid, name) {
+    this.uid = uid;
+    this.name = name;
+  }
+
+  getUid() {
+    return this.uid;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+}
