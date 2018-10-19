8c3379bf07e17beec144e865193c8e625c7d3411
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2018-10-19 21:07:12 +0200

    Ajout de la méthode getHiddenCards()

--- src/model/CardPack.js
+++ src/model/CardPack.js
@@ -28,13 +28,38 @@ class CardPack {
     return this.pack.pop();
   }
 
-  // Renvoie plusieurs cartes pour le début de partie
+  // Renvoie plusieurs cartes
   getManyCards(how) {
     let cards = [];
     for (let i = 0; i < how; i++) {
       cards[i] = this.getFollowingCard();
     }
     return cards;
+  }
+
+  // Retire du paquet et renvoie une carte de chaque type pour le début de partie
+  getHiddenCards() {
+    let hiddenCharacter, hiddenWeapon, hiddenRoom;
+    for (let i in this.pack) {
+      // On récupère une carte de chaque type dans le paquet
+      let card = this.pack[i];
+      if (hiddenCharacter === undefined && card.getType() === "character") {
+        hiddenCharacter = this.pack.splice(i, 1);
+      } else if (hiddenRoom === undefined && card.getType() === "room") {
+        hiddenRoom = this.pack.splice(i, 1);
+      } else if (hiddenWeapon === undefined && card.getType() === "weapon") {
+        hiddenWeapon = this.pack.splice(i, 1);
+      }
+
+      // Si une carte de chaque type est prise, les renvoier
+      if (
+        hiddenCards != undefined &&
+        hiddenCharacter != undefined &&
+        hiddenRoom != undefined
+      ) {
+        return hiddenCharacter + hiddenRoom + hiddenWeapon;
+      }
+    }
   }
 
   // Mélange le jeu
