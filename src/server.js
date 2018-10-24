49397ad5b8e98c05f0cb0653769c66deeb530273
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2018-10-25 00:02:05 +0200

    Implémentation de la classe Game

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const expressSession = require("express-session");
 const sharedSession = require("express-socket.io-session");
 const uuid = require("uuid/v4");
 
+const Game = require("./class/Game");
+
 // Data
 const config = require("./config.json");
 const cards = require("./cards.json");
@@ -19,6 +21,7 @@ let session = expressSession({
   resave: true,
   saveUninitialized: true
 });
+let game = new Game();
 
 app.set("view engine", "twig");
 app.set("views", "./src/views");
@@ -26,8 +29,6 @@ app.set("views", "./src/views");
 // MIDDLEWARES
 // attachement de la session
 app.use(session);
-// partage de la session avec socketio
-io.use(sharedSession(session));
 // serveur de fichier statiques
 app.use(
   config.ressources.staticFilesRootPath,
@@ -35,8 +36,16 @@ app.use(
 );
 
 // WEBSOCKET
+// partage de la session avec socketio
+let serverSocket = io.of("/cluedo");
+serverSocket.use(
+  sharedSession(session, {
+    autoSave: true
+  })
+);
+
 // lorsqu'un client se connecte à la socket
-io.on("connection", clientSocket => {
+serverSocket.on("connection", clientSocket => {
   // on stocke les infos du joueur
   clientSocket.handshake.session.userData = {
     uid: uuid(),
@@ -45,8 +54,21 @@ io.on("connection", clientSocket => {
 
   // écoute des évènements
   clientSocket.on("join-game", () => {
-    const user = clientSocket.handshake.session.userData;
-    console.log(user.uid + " - " + user.name);
+    const player = clientSocket.handshake.session.userData;
+    try {
+      game.joinGame(player);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  clientSocket.on("disconnect", () => {
+    const player = clientSocket.handshake.session.userData;
+    try {
+      game.quitGame(player);
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
 
